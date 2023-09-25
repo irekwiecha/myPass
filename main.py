07b71599d781c31f183eab3addd9c463f6c51f57
irekwiecha <irek88@gmail.com>
2023-09-25 21:35:46 +0200

    chore: initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = tk.Tk()
+window.title("MyPass")
+window.config(padx=20, pady=20)
+
+canvas = tk.Canvas(width=200, height=200)
+logo_img = tk.PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.pack()
+
+
+
+window.mainloop()
