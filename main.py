c775058415fb1d6fe0e0078802c065427b9e0ba1
irekwiecha <irek88@gmail.com>
2023-09-26 21:56:36 +0200

    feat: save data to text file on local machine

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import tkinter as tk
 
+EMAIL = "your@address.com"
+ACTIVE_BUTTON = "#9bdeac"
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_data():
+    website = website_entry.get()
+    username = website_entry.get()
+    password = password_entry.get()
+    with open("data.txt", mode="a") as f:
+        f.write(f"{website} | {username} | {password}\n")
+    website_entry.delete(0, "end")
+    password_entry.delete(0, "end")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = tk.Tk()
@@ -18,23 +31,22 @@ website_label = tk.Label(text="Website:")
 website_label.grid(row=1, column=0)
 website_entry = tk.Entry(width=52)
 website_entry.grid(row=1, column=1, columnspan=2)
-website = website_entry.get()
+website_entry.focus()
 
 username_label = tk.Label(text="Email/Username:")
 username_label.grid(row=2, column=0)
 username_entry = tk.Entry(width=52)
 username_entry.grid(row=2, column=1, columnspan=2)
-username = website_entry.get()
+username_entry.insert(0, EMAIL)
 
 password_label = tk.Label(text="Password:")
 password_label.grid(row=3, column=0)
 password_entry = tk.Entry(width=33)
 password_entry.grid(row=3, column=1)
-password_button = tk.Button(text="Generate Password")
+password_button = tk.Button(text="Generate Password", activebackground=ACTIVE_BUTTON)
 password_button.grid(row=3, column=2)
-password = password_entry.get()
 
-add_button = tk.Button(text="Add", width=44)
+add_button = tk.Button(text="Add", width=44, command=save_data, activebackground=ACTIVE_BUTTON)
 add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
